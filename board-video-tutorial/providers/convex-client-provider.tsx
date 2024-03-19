d69d9070d126bbfc7cc821f58b7ec00672704e24
Alexander.Nees <alexander.nees@valocityglobal.com>
2024-03-19 17:38:39 +1300

    Add basic layout

--- board-video-tutorial/providers/convex-client-provider.tsx
+++ board-video-tutorial/providers/convex-client-provider.tsx
@@ -1,25 +1,26 @@
-"use client";
+'use client';
 
-import { ClerkProvider, useAuth } from "@clerk/nextjs";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { AuthLoading, Authenticated, ConvexReactClient} from "convex/react";
-import { Loading } from "@/components/auth/loading";
+import {ClerkProvider, useAuth} from '@clerk/nextjs';
+import {ConvexProviderWithClerk} from 'convex/react-clerk';
+import {AuthLoading, Authenticated, ConvexReactClient} from 'convex/react';
+import {Loading} from '@/components/auth/loading';
 
 interface ConvexClientProviderProps {
   children: React.ReactNode;
 }
 
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
-const clerkPublishableKey = process.env.CLERK_PUBLISHABLE_KEY!;
+const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 const convex = new ConvexReactClient(convexUrl);
 
-export const ConvexClientProvider = ({ children }: ConvexClientProviderProps) => {
+export const ConvexClientProvider = ({children}: ConvexClientProviderProps) => {
   return (
     <ClerkProvider publishableKey={clerkPublishableKey}>
-      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
-        <Authenticated>
-          {children}
-        </Authenticated>
+      <ConvexProviderWithClerk
+        useAuth={useAuth}
+        client={convex}
+      >
+        <Authenticated>{children}</Authenticated>
         <AuthLoading>
           <Loading />
         </AuthLoading>
