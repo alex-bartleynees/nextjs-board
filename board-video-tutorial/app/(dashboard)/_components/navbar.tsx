a71849c0b2947603db88d0b7fb28896ae10120d2
alexbn <alexbartleynees@gmail.com>
2024-03-28 08:07:58 +1300

    Update navbar

--- board-video-tutorial/app/(dashboard)/_components/navbar.tsx
+++ board-video-tutorial/app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,40 @@
-'use client';
+"use client";
 
-import {UserButton} from '@clerk/nextjs';
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { SearchInput } from "./sidebar/search-input";
+import { InviteButton } from "./invite-button";
+import { useOrganization } from "@clerk/nextjs";
 
 export const Navbar = () => {
+  const { organization } = useOrganization();
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg:flex lg:flex-1">Search</div>
+    <div className="flex items-center gap-x-4 p-5">
+      <SearchInput />
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                maxWidth: "376px",
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                borderRadius: "8px",
+                border: "1px solid #e5e7eb",
+                justifyContent: "space-between",
+                backgroundColor: "#fff",
+              },
+            },
+          }}
+        />
+      </div>
+      {organization && <InviteButton />}
       <UserButton />
     </div>
   );
